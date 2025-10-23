a7daebc869739ab7693ba55e9ea21f538f2d4003
phoebemiller-jbh <phoebe.miller@jbhunt.com>
2025-10-23 13:27:53 -0500

    Add files via upload

---
+++ code/src/visual.ts
@@ -0,0 +1,26 @@
+import "core-js/stable";
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
+import VisualObjectInstance = powerbi.VisualObjectInstance;
+import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
+
+import { Visual as Routemap } from './routemap/visual';
+
+export class Visual implements IVisual {
+    private _visual: IVisual;
+    constructor(options: VisualConstructorOptions) {
+        this._visual = new Routemap(options);
+    }
+
+    public update(options: VisualUpdateOptions) {
+        this._visual.update(options);
+    }
+
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
+        return this._visual.enumerateObjectInstances(options);
+    }
+}
